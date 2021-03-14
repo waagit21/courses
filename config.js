76211f8ecef7871da40c7bae20d2bd63e2afdeb1
thewaa <wqr.iiui@gmail.com>
2021-03-15 00:22:35 +0500

    config

--- config.js
+++ config.js
@@ -10,12 +10,12 @@ connectDB=async()=>{
       mongoose.set('useNewUrlParser', true);
       mongoose.set('useFindAndModify', false);
       mongoose.set('useCreateIndex', true);
-      // await mongoose.connect(
-      //   "mongodb+srv://course:zpCFJE8mq3Uk9y05@cluster0.jlhhi.mongodb.net/coursedb?retryWrites=true&w=majority",options
-      // )
       await mongoose.connect(
-        "mongodb://localhost:27017/classonline",options
+        "mongodb+srv://course:zpCFJE8mq3Uk9y05@cluster0.jlhhi.mongodb.net/coursedb?retryWrites=true&w=majority",options
       )
+      // await mongoose.connect(
+      //   "mongodb://localhost:27017/classonline",options
+      // )
       console.log("Mongo Connected");
     }catch(err){
       console.error('here is the error',err);
