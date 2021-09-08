1cefaa448530a22ef45f44806a798a0bdbafdbf9
thewaa2 <wqr.iiui@gmail.com>
2021-09-09 01:27:23 +0500

    course

--- config.js
+++ config.js
@@ -10,12 +10,12 @@ connectDB=async()=>{
       mongoose.set('useNewUrlParser', true);
       mongoose.set('useFindAndModify', false);
       mongoose.set('useCreateIndex', true);
-      await mongoose.connect(
-        "mongodb+srv://BabarKhan:babar123@cluster0.xikk7.mongodb.net/CourseDb?retryWrites=true&w=majority",options
-      )
       // await mongoose.connect(
-      //   "mongodb+srv://course:zpCFJE8mq3Uk9y05@cluster0.jlhhi.mongodb.net/coursedb?retryWrites=true&w=majority",options
+      //   "mongodb+srv://BabarKhan:babar123@cluster0.xikk7.mongodb.net/CourseDb?retryWrites=true&w=majority",options
       // )
+      await mongoose.connect(
+        "mongodb+srv://course:zpCFJE8mq3Uk9y05@cluster0.jlhhi.mongodb.net/coursedb?retryWrites=true&w=majority",options
+      )
       // await mongoose.connect(
       //   "mongodb://localhost:27017/classonline",options
       // )
